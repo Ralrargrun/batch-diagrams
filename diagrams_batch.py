fdd9b74cb19c656a0cbf9cb61934e74b509c83be
Leonilson Alencar <86755623+Ralrargrun@users.noreply.github.com>
2024-11-20 19:01:27 -0300

    up

--- diagrams_batch.py
+++ diagrams_batch.py
@@ -1,11 +1,17 @@
-from diagrams import Diagram
+from diagrams import Cluster,Diagram
 from diagrams.aws.compute import EC2
+from diagrams.aws.analytics import Glue
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("Grouped Workers", show=False, direction="TB"):
-    ELB("lb") >> [EC2("worker1"),
-                  EC2("worker2"),
-                  EC2("worker3"),
-                  EC2("worker4"),
-                  EC2("worker5")] >> RDS("events")
+with Diagram("Grouped Workers", show=False, direction="TB", outformat="svg"):
+    
+    with Cluster("Step 1",graph_attr={'margin':0}):
+       glue1 = Glue("hsuadhususahdusahdu",nodeid="xablau",href="xablau", target="_blank") 
+       glue2 = Glue("hsuadhususahdusahdu",nodeid="xablau2",href="xablau", target="_blank") 
+
+    with Cluster("Step 2"):
+       glue3 = Glue("hsuadhususahdusahdu",nodeid="xablau3",href="xablau", target="_blank") 
+       glue4 = Glue("hsuadhususahdusahdu",nodeid="xablau4",href="xablau", target="_blank") 
+    
+    glue1 >> glue3
