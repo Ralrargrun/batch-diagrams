61ab280271b5b5bb67b693a50c97adc3700bf705
Leonilson Alencar <86755623+Ralrargrun@users.noreply.github.com>
2024-12-19 22:25:20 -0300

    up

--- diagrams_batch.py
+++ diagrams_batch.py
@@ -6,12 +6,16 @@ from diagrams.aws.network import ELB
 
 with Diagram("Grouped Workers", show=False, direction="TB", outformat="svg"):
     
-    with Cluster("Step 1",graph_attr={'margin':'50,200'}):
+    with Cluster("Step 1",graph_attr={'margin': "30"}):
        glue1 = Glue("hsuadhususahdusahdu",nodeid="xablau",href="xablau", target="_blank") 
        glue2 = Glue("hsuadhususahdusahdu",nodeid="xablau2",href="xablau", target="_blank") 
 
-    with Cluster("Step 2"):
+    with Cluster("Step 2",graph_attr={'margin': "30"}):
        glue3 = Glue("hsuadhususahdusahdu",nodeid="xablau3",href="xablau", target="_blank") 
        glue4 = Glue("hsuadhususahdusahdu",nodeid="xablau4",href="xablau", target="_blank") 
+       glue5 = Glue("hsuadhususahdusahdu",nodeid="xablau5",href="xablau", target="_blank") 
+       glue6 = Glue("hsuadhususahdusahdu",nodeid="xablau6",href="xablau", target="_blank") 
+       glue7 = Glue("hsuadhususahdusahdu",nodeid="xablau7",href="xablau", target="_blank") 
+       glue8 = Glue("hsuadhususahdusahdu",nodeid="xablau8",href="xablau", target="_blank") 
     
     glue1 >> glue3
